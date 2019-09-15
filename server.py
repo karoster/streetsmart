23c8406a68c39b7a854137a523deced2579b1c02
Christian Cosgrove <ccosgro2@jhu.edu>
2019-09-15 03:22:10 -0400

    host and port

--- server.py
+++ server.py
@@ -22,4 +22,4 @@ def index():
     return render_template('index.html', title='Home', folium=graph_map._repr_html_())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
